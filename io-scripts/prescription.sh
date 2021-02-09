8c0afef59ceb0a44647235950edfb6e7a880dbbc
Rahul Gunasekaran <71350204+rahulguna10@users.noreply.github.com>
2021-02-09 17:47:47 -0500

    Update prescription.sh

--- io-scripts/prescription.sh
+++ io-scripts/prescription.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 run() {
-    box_line "Synopsys Intelligent Security Scan" "Copyright © 2016-2020 Synopsys, Inc. All rights reserved worldwide."
+    box_line "Synopsys Intelligent Security Scan" "Copyright 2016-2020 Synopsys, Inc. All rights reserved worldwide."
     allargs="${ARGS[@]}"
-
+    
+    config_file="io-manifest.yml"
+    
     for i in "${ARGS[@]}"; do
         case "$i" in
         --stage=*) stage="${i#*=}" ;;
@@ -16,9 +18,12 @@ run() {
     validate_values "STAGE" "$stage"
 
     box_star "Current Stage is set to ${stage}"
+
+    #method to generate synopsys-io.yml file
+    generateYML "${ARGS[@]}"
     
     if [[ "${stage}" == "IO" ]]; then
-        getPrescription "${ARGS[@]}"
+        getIOPrescription "${ARGS[@]}"
     elif [[ "${stage}" == "WORKFLOW" ]]; then
         loadWorkflow "${ARGS[@]}"
     else
@@ -26,23 +31,33 @@ run() {
     fi
 }
 
-function loadWorkflow() {
+function generateYML () {
     for i in "$@"; do
         case "$i" in
-        --IO.url=*) url="${i#*=}" ;;
-        --IO.token=*) authtoken="${i#*=}" ;;
+        --io.url=*) io_url="${i#*=}" ;;
+        --io.token=*) io_token="${i#*=}" ;;
+	--io.manifest.url=*) io_manifest_url="${i#*=}" ;;
+        --release.type=*) release_type="${i#*=}" ;;
+	--file.change.threshold=*) file_change_threshold="${i#*=}" ;;
+	--sast.rescan.threshold=*) sast_rescan_threshold="${i#*=}" ;;
+	--sca.rescan.threshold=*) sca_rescan_threshold="${i#*=}" ;;
         --workflow.url=*) workflow_url="${i#*=}" ;;
         --workflow.token=*) workflow_token="${i#*=}" ;;
-        --workflow.template=*) workflow_file="${i#*=}" ;;
+	--sensitive.package.pattern=*) sensitive_package="${i#*=}" ;;
+        --asset.id=*) asset_id="${i#*=}" ;;
         --slack.channel.id=*) slack_channel_id="${i#*=}" ;;    #slack
         --slack.token=*) slack_token="${i#*=}" ;;
-        --jira.project.key=*) jira_project_key="${i#*=}" ;;    #jira
+        --jira.project.name=*) jira_project_name="${i#*=}" ;;    #jira
         --jira.assignee=*) jira_assignee="${i#*=}" ;;
-        --jira.url=*) jira_server_url="${i#*=}" ;;
+        --jira.api.url=*) jira_api_url="${i#*=}" ;;
+	--jira.issues.query=*) jira_issues_query="${i#*=}" ;;
         --jira.username=*) jira_username="${i#*=}" ;;
-        --jira.token=*) jira_auth_token="${i#*=}" ;;
-        --bitbucket.workspace.name=*) bitbucket_workspace_name="${i#*=}" ;;    #bitbucket
-        --bitbucket.repository.name=*) bitbucket_repo_name="${i#*=}" ;;
+        --jira.auth.token=*) jira_auth_token="${i#*=}" ;;
+        --scm.type=*) scm_type="${i#*=}" ;;    #scm
+        --scm.owner=*) scm_owner="${i#*=}" ;;
+        --scm.repo.name=*) scm_repo_name="${i#*=}" ;;
+        --scm.branch.name=*) scm_branch_name="${i#*=}" ;;
+        --bitbucket.workspace=*) bitbucket_workspace="${i#*=}" ;;    #bitbucket
         --bitbucket.commit.id=*) bitbucket_commit_id="${i#*=}" ;;
         --bitbucket.username=*) bitbucket_username="${i#*=}" ;;
         --bitbucket.password=*) bitbucket_password="${i#*=}" ;;
@@ -60,56 +75,93 @@ function loadWorkflow() {
         --blackduck.project.name=*) blackduck_project_name="${i#*=}" ;;
         --blackduck.url=*) blackduck_server_url="${i#*=}" ;;
         --blackduck.api.token=*) blackduck_access_token="${i#*=}" ;;
+        --persona=*) persona="${i#*=}" ;;
         *) ;;
         esac
     done
     
-    #checks if the manifest files are present
-    is_workflow_manifest_present
-	
+    validate_values "IO_SERVER_URL" "$io_url"
+    validate_values "IO_SERVER_TOKEN" "$io_token"
+    
+    #checks if the synopsys-io.yml present
+    is_synopsys_config_present
+
+    asset_id_from_yml=$(ruby -r yaml -e 'puts YAML.load_file(ARGV[0])["application"]["assetId"]' $config_file)
+
+    if [[ "${asset_id_from_yml}" == "<<ASSET_ID>>" ]]; then
+        create_io_asset
+    else
+        asset_id=${asset_id_from_yml}
+    fi
+    
+    if [ -z "$file_change_threshold" ]; then
+        file_change_threshold=20
+    fi
+    
+    if [ -z "$sast_rescan_threshold" ]; then
+        sast_rescan_threshold=10
+    fi
+    
+    if [ -z "$sca_rescan_threshold" ]; then
+        sca_rescan_threshold=10
+    fi
+
+    synopsys_io_manifest=$(cat io-manifest.yml |
+        sed " s~<<SLACK_CHANNEL_ID>>~$slack_channel_id~g; \
+	    s~<<SLACK_TOKEN>>~$slack_token~g; \
+	    s~<<JIRA_PROJECT_NAME>>~$jira_project_name~g; \
+	    s~<<JIRA_ASSIGNEE>>~$jira_assignee~g; \
+	    s~<<JIRA_API_URL>>~$jira_api_url~g; \
+	    s~<<JIRA_ISSUES_QUERY>>~$jira_issues_query~g; \
+	    s~<<JIRA_USERNAME>>~$jira_username~g; \
+	    s~<<JIRA_AUTH_TOKEN>>~$jira_auth_token~g; \
+	    s~<<BITBUCKET_COMMIT_ID>>~$bitbucket_commit_id~g; \
+	    s~<<BITBUCKET_USERNAME>>~$bitbucket_username~g; \
+	    s~<<BITBUCKET_PASSWORD>>~$bitbucket_password~g; \
+	    s~<<GITHUB_OWNER_NAME>>~$github_owner_name~g; \
+	    s~<<GITHUB_REPO_NAME>>~$github_repo_name~g; \
+	    s~<<GITHUB_REF>>~$github_ref~g; \
+	    s~<<GITHUB_COMMIT_ID>>~$github_commit_id~g; \
+	    s~<<GITHUB_USERNAME>>~$github_username~g; \
+	    s~<<GITHUB_ACCESS_TOKEN>>~$github_access_token~g; \
+	    s~<<POLARIS_PROJECT_NAME>>~$polaris_project_name~g; \
+	    s~<<POLARIS_SERVER_URL>>~$polaris_server_url~g; \
+	    s~<<POLARIS_ACCESS_TOKEN>>~$polaris_access_token~g; \
+	    s~<<BLACKDUCK_PROJECT_NAME>>~$blackduck_project_name~g; \
+	    s~<<BLACKDUCK_SERVER_URL>>~$blackduck_server_url~g; \
+	    s~<<BLACKDUCK_ACCESS_TOKEN>>~$blackduck_access_token~g; \
+            s~<<IS_SAST_ENABLED>>~$is_sast_enabled~g; \
+            s~<<IS_SCA_ENABLED>>~$is_sca_enabled~g; \
+	    s~<<APP_ID>>~$asset_id~g; \
+	    s~<<ASSET_ID>>~$asset_id~g; \
+            s~<<RELEASE_TYPE>>~$release_type~g; \
+	    s~<<SENSITIVE_PACKAGE_PATTERN>>~$sensitive_package~g; \
+	    s~<<FILE_CHANGE_THRESHOLD>>~$file_change_threshold~g; \
+	    s~<<SAST_RESCAN_THRESHOLD>>~$sast_rescan_threshold~g; \
+	    s~<<SCA_RESCAN_THRESHOLD>>~$sca_rescan_threshold~g; \
+	    s~<<SCM_TYPE>>~$scm_type~g; \
+	    s~<<SCM_OWNER>>~$scm_owner~g; \
+	    s~<<SCM_REPO_NAME>>~$scm_repo_name~g; \
+	    s~<<SCM_BRANCH_NAME>>~$scm_branch_name~g")
+    
+    # apply the yml with the substituted value
+    echo "$synopsys_io_manifest" >synopsys-io.yml
+
+    echo "synopsys-io.yml generated"
+}
+
+function loadWorkflow() {
     #validates mandatory arguments for IO
-    validate_values "IO_SERVER_URL" "$url"
-    validate_values "IO_SERVER_TOKEN" "$authtoken"
     validate_values "WORKFLOW_SERVER_URL" "$workflow_url"
     validate_values "WORKFLOW_SERVER_TOKEN" "$workflow_token"
-	
-    echo "Editing Workflow Template"
-    # read the workflow.yml from a file and substitute the string
-    # {{MYVARNAME}} with the value of the MYVARVALUE variable
-    workflow=$(cat $workflow_file |
-        sed " s~<<SLACK_CHANNEL_ID>>~$slack_channel_id~g; \
-    s~<<SLACK_TOKEN>>~$slack_token~g; \
-    s~<<JIRA_PROJECT_KEY>>~$jira_project_key~g; \
-    s~<<JIRA_ASSIGNEE>>~$jira_assignee~g; \
-    s~<<JIRA_SERVER_URL>>~$jira_server_url~g; \
-    s~<<JIRA_USERNAME>>~$jira_username~g; \
-    s~<<JIRA_AUTH_TOKEN>>~$jira_auth_token~g; \
-    s~<<BITBUCKET_WORKSPACE_NAME>>~$bitbucket_workspace_name~g; \
-    s~<<BITBUCKET_REPO_NAME>>~$bitbucket_repo_name~g; \
-    s~<<BITBUCKET_COMMIT_ID>>~$bitbucket_commit_id~g; \
-    s~<<BITBUCKET_USERNAME>>~$bitbucket_username~g; \
-    s~<<BITBUCKET_PASSWORD>>~$bitbucket_password~g; \
-    s~<<GITHUB_OWNER_NAME>>~$github_owner_name~g; \
-    s~<<GITHUB_REPO_NAME>>~$github_repo_name~g; \
-    s~<<GITHUB_REF>>~$github_ref~g; \
-    s~<<GITHUB_COMMIT_ID>>~$github_commit_id~g; \
-    s~<<GITHUB_USERNAME>>~$github_username~g; \
-    s~<<GITHUB_ACCESS_TOKEN>>~$github_access_token~g; \
-    s~<<IS_SAST_ENABLED>>~$is_sast_enabled~g; \
-    s~<<POLARIS_PROJECT_NAME>>~$polaris_project_name~g; \
-    s~<<POLARIS_SERVER_URL>>~$polaris_server_url~g; \
-    s~<<POLARIS_ACCESS_TOKEN>>~$polaris_access_token~g; \
-    s~<<IS_SCA_ENABLED>>~$is_sca_enabled~g; \
-    s~<<BLACKDUCK_PROJECT_NAME>>~$blackduck_project_name~g; \
-    s~<<BLACKDUCK_SERVER_URL>>~$blackduck_server_url~g; \
-    s~<<BLACKDUCK_ACCESS_TOKEN>>~$blackduck_access_token~g")
-    # apply the yml with the substituted value
-    echo "$workflow" >synopsys-io-workflow.yml
-	
-    io_assetId=$(ruby -r yaml -e 'puts YAML.load_file(ARGV[0])["application"]["assetId"]' ApplicationManifest.yml)
+    
+    #checks if WorkflowClient.jar is present
+    is_workflow_client_jar_present
+    
+    asset_id_from_yml=$(ruby -r yaml -e 'puts YAML.load_file(ARGV[0])["application"]["assetId"]' synopsys-io.yml)
     curr_date=$(date +'%Y-%m-%d')
    
-    scandate_json="{\"assetId\": \"${io_assetId}\",\"activities\":{"
+    scandate_json="{\"assetId\": \"${asset_id_from_yml}\",\"activities\":{"
     if [ "$is_sast_enabled" = true ] ; then
        scandate_json="$scandate_json\"sast\": {\"lastScanDate\": \"${curr_date}\"}"
     fi
@@ -123,68 +175,45 @@ function loadWorkflow() {
     echo "$scandate_json" >scandate.json
     echo "$scandate_json"
 	
-    header='Authorization: Bearer '$authtoken''
-    scandateresponse=$(curl -X POST -H 'Content-Type:application/json' -H 'Accept:application/json' -H "${header}" -d @scandate.json ${url}/stargazer/api/manifest/update/scandate)
+    echo "updating last scan date for perfomed security activities"
+    header='Authorization: Bearer '$io_token''
+    scandateresponse=$(curl -X POST -H 'Content-Type:application/json' -H 'Accept:application/json' -H "${header}" -d @scandate.json ${io_url}/stargazer/api/manifest/update/scandate)
     echo $scandateresponse
 }
 
-function getPrescription() {
+function getIOPrescription() {
 
     echo "Inside Prescription"
-    echo "$@"
-
-    for i in "$@"; do
-        case "$i " in
-        --IO.url=*) url="${i#*=}" ;;
-        --IO.token=*) authtoken="${i#*=}" ;;
-        --app.manifest.path=*) afile="${i#*=}" ;;
-        --sec.manifest.path=*) sfile="${i#*=}" ;;
-        --persona=*) persona="${i#*=}" ;;
-        *) ;;
-        esac
-    done
-    
-    #checks if the manifest files are present
-    is_io_manifest_present
 	
     #validates mandatory arguments for IO
-    validate_values "IO_SERVER_URL" "$url"
-    validate_values "IO_SERVER_TOKEN" "$authtoken"
+    validate_values "IO_ASSET_ID" "$asset_id"
+    validate_values "SCM_TYPE" "$scm_type"
+    validate_values "SCM_OWNER" "$scm_owner"
+    validate_values "REPOSITORY_NAME" "$scm_repo_name"
+    validate_values "BRANCH_NAME" "$scm_branch_name"
+    
+    printf "IO Asset ID: ${asset_id}\n"
+    printf "SCM TYPE: ${scm_type}\n"
+    printf "Using the repository ${scm_repo_name} and branch ${scm_branch_name}. Action triggered by ${scm_owner}\n\n"
 	
     #chosing API - if persona is set to "developer" then "/api/manifest/update/persona/developer" will be called
     #chosing API - if persona is empty then "/api/manifest/update" will be called
     if [[ -z $persona ]]; then
         API="update"
     else
-        API="update/persona/developer"
+        API="update/persona/$persona"
     fi
 
-    echo $afile
-    echo $sfile
+    header='Authorization: Bearer '$io_token''
 
-    header='Authorization: Bearer '$authtoken''
-
-    if [[ $afile == *.yml && $sfile == *.yml ]]; then
-        cat $afile $sfile >>merge.yml
-        #Yaml to Json Conversion
-        echo $(ruby -ryaml -rjson -e "puts JSON.pretty_generate(YAML.safe_load(File.read('merge.yml')))") >>data.json
-        echo "Getting Prescription"
-        prescrip=$(curl -X POST -H 'Content-Type:application/json' -H 'Accept:application/json' -H "${header}" -d @data.json ${url}/stargazer/api/manifest/${API})
-        echo $prescrip
-        echo $prescrip >>result.json
-    else
-        exit_program "Invalid Files"
-    fi
-
-    echo "::set-output name=sastScan::$(ruby -rjson -e 'j = JSON.parse(File.read("result.json")); puts j["security"]["activities"]["sast"]["enabled"]')"
-    echo "::set-output name=scaScan::$(ruby -rjson -e 'j = JSON.parse(File.read("result.json")); puts j["security"]["activities"]["sca"]["enabled"]')"
-    echo "::set-output name=dastScan::$(ruby -rjson -e 'j = JSON.parse(File.read("result.json")); puts j["security"]["activities"]["dast"]["enabled"]')"
-
-    echo "Cleaning Up"
-    rm -r data.json
-    rm -r merge.yml
-    rm -r result.json
-
+    #Yaml to Json Conversion
+    cat synopsys-io.yml
+    echo $(ruby -ryaml -rjson -e "puts JSON.pretty_generate(YAML.safe_load(File.read('synopsys-io.yml')))") >data.json
+    cat data.json
+    echo "Getting Prescription"
+    prescrip=$(curl -X POST -H 'Content-Type:application/json' -H 'Accept:application/json' -H "${header}" -d @data.json ${io_url}/stargazer/api/manifest/${API})
+    echo $prescrip
+    echo $prescrip >result.json
 }
 
 function validate_values () {
@@ -195,31 +224,51 @@ function validate_values () {
     fi
 }
 
-function is_io_manifest_present () {
-    if [ ! -f "ApplicationManifest.yml" ]; then
-        exit_program "ApplicationManifest.yml file does not exist"	
-    fi
-    if [ ! -f "SecurityManifest.yml" ]; then
-        printf "SecurityManifest.yml file does not exist\n"
-        printf "Downloading default SecurityManifest.yml\n"
-        wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/${workflow_version}/SecurityManifest.yml
+function is_synopsys_config_present () {
+    if [ ! -f "$config_file" ]; then
+        printf "${config_file} file does not exist\n"
+        printf "Downloading default ${config_file}\n"
+        if [ -z "$io_manifest_url" ]; then
+            wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/${workflow_version}/io-manifest.yml
+        else
+            wget "$io_manifest_url"
+	fi
     fi
 }
 
-function is_workflow_manifest_present () {
-    if [ ! -f "ApplicationManifest.yml" ]; then
-        exit_program "ApplicationManifest.yml file does not exist"
-    fi
-    if [ ! -f "WorkflowTemplate.yml" ]; then
-        printf "WorkflowTemplate.yml file does not exist\n"
-        printf "Downloading default WorkflowTemplate.yml\n"
-        wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/${workflow_version}/WorkflowTemplate.yml
-    fi
+function is_workflow_client_jar_present () {
     if [ ! -f "WorkflowClient.jar" ]; then
         printf "WorkflowClient.jar file does not exist\n"
         printf "Downloading default WorkflowClient.jar\n"
         wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/${workflow_version}/WorkflowClient.jar
     fi
+}
+
+function create_io_asset () {	
+    validate_values "IO_SERVER_URL" "$io_url"
+    validate_values "IO_SERVER_TOKEN" "$io_token"
+    validate_values "IO_ASSET_ID" "$asset_id"
+
+    onBoardingResponse=$(curl --location --request POST "$io_url/stargazer/api/applications/update" \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $io_token" \
+    --data-raw '{
+        "assetId": '\"$asset_id\"',
+        "assetType": "Application",
+        "applicationType": "Financial",
+        "applicationName": "Test app 1",
+        "applicationBuildName": "test-build",
+        "soxFinancial": true,
+        "ppi": true,
+        "mnpi": true,
+        "infoClass": "Restricted",
+        "customerFacing": true,
+        "externallyFacing": true,
+        "assetTier": "Tier 01",
+        "fairLending": true
+    }');
+
+    echo $onBoardingResponse
 }
 
 function box_line () {
@@ -228,7 +277,7 @@ function box_line () {
     len=$((${#arg2}+5))
     box_str="\n+"
     for i in $(seq $len); do box_str="$box_str-"; done;
-    box_str="$box_str+\n| "$arg1" "$(printf '%*s' 35)" |\n"
+    box_str="$box_str+\n| "$arg1" "$(printf '%*s' 33)" |\n"
     box_str="$box_str| "$arg2" "$(printf '%*s' 2)" |\n+"
     for i in $(seq $len); do box_str="$box_str-"; done;
     box_str="$box_str+\n\n"
