7fcd5dbd27d5803b0224e10c8e1f5b08b4da7558
akshayme-synp <71349056+akshayme-synp@users.noreply.github.com>
2021-02-10 12:58:46 -0500

    Update get-io-token.sh

--- io-scripts/get-io-token.sh
+++ io-scripts/get-io-token.sh
@@ -3,14 +3,13 @@
 for i in "$@"; do
     case "$i" in
     --io.url=*) io_url="${i#*=}" ;;
-    --workflow.engine.url=*) workflow_url="${i#*=}" ;;
     --username=*) userName="${i#*=}" ;;
     --password=*) password="${i#*=}" ;;
     *) ;;
     esac
 done
 
-signupResponse=$(curl --location --request POST "$io_url/stargazer/user/signup" \
+signupResponse=$(curl --location --request POST "$io_url/io/user/signup" \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "userName": '\"$userName\"',
@@ -18,22 +17,7 @@ signupResponse=$(curl --location --request POST "$io_url/stargazer/user/signup" 
     "confirmPassword": '\"$password\"'
 }');
 
-userToken=$(curl --location --request POST "$io_url/stargazer/user/token" \
---header 'Content-Type: application/json' \
---data-raw '{
-	"userName": '\"$userName\"',
-	"password": '\"$password\"'
-}');
-
-workflowSignupResponse=$(curl --location --request POST "$workflow_url/sct/user/signup" \
---header 'Content-Type: application/json' \
---data-raw '{
-	"userName": '\"$userName\"',
-	"password": '\"$password\"',
-	"confirmPassword": '\"$password\"'
-}');
-
-workflowUserToken=$(curl --location --request POST "$workflow_url/sct/user/token" \
+userToken=$(curl --location --request POST "$io_url/io/user/token" \
 --header 'Content-Type: application/json' \
 --data-raw '{
 	"userName": '\"$userName\"',
@@ -41,5 +25,4 @@ workflowUserToken=$(curl --location --request POST "$workflow_url/sct/user/token
 }');
 
 echo "IO_ACCESS_TOKEN: $userToken"
-echo "WORKFLOW_ENGINE_ACCESS_TOKEN: $workflowUserToken"
 
