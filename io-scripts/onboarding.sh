2481b88b505039929a3ae4d35cbd01728506445d
akshayme-synp <71349056+akshayme-synp@users.noreply.github.com>
2021-02-10 12:56:58 -0500

    Update onboarding.sh

--- io-scripts/onboarding.sh
+++ io-scripts/onboarding.sh
@@ -18,50 +18,46 @@ signupResponse=$(curl --location --request POST "$io_url/io/user/signup" \
     "confirmPassword": '\"$password\"'
 }');
 
-if [ "$signupResponse" = "New user created with a token" ] ; then
-    userToken=$(curl --location --request POST "$io_url/io/user/token" \
-    --header 'Content-Type: application/json' \
-    --data-raw '{
-        "userName": '\"$userName\"',
-        "password": '\"$password\"'
-    }');
-    echo $userToken;
-    
-    onBoardingResponse=$(curl --location --request POST "$io_url/io/api/applications/update" \
-    --header 'Content-Type: application/json' \
-    --header "Authorization: Bearer $userToken" \
-    --data-raw '{
-        "assetId": '\"$assetId\"',
-        "assetType": "Application",
-        "applicationType": "Financial",
-        "applicationName": "Test app 1",
-        "applicationBuildName": "test-build",
-        "soxFinancial": true,
-        "ppi": true,
-        "mnpi": true,
-        "infoClass": "Restricted",
-        "customerFacing": true,
-        "externallyFacing": true,
-        "assetTier": "Tier 01",
-        "fairLending": true
-    }');
+userToken=$(curl --location --request POST "$io_url/io/user/token" \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "userName": '\"$userName\"',
+    "password": '\"$password\"'
+}');
+echo $userToken;
 
-    if [ "$onBoardingResponse" = "TPI Data created/updated successfully" ] ; then
-        wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/2021.01/io-manifest.yml
-        workflow=$(cat io-manifest.yml | sed " s~<<ASSET_ID>>~$assetId~g; s~<<APP_ID>>~$assetId~g")
-        # apply the yml with the substituted value
-        echo "$workflow" >io-manifest.yml
-		
-        echo "Save the following values for further use:"
-        echo "assetId: $assetId"
-        echo "IO_ACCESS_TOKEN: $userToken"
-        echo "INFO: io-manifest.yml is generated. Please update the source code management details in it and add the file to the root of the project."
-    else
-        echo $onBoardingResponse;
-        exit 1;
-    fi
+onBoardingResponse=$(curl --location --request POST "$io_url/io/api/applications/update" \
+--header 'Content-Type: application/json' \
+--header "Authorization: Bearer $userToken" \
+--data-raw '{
+    "assetId": '\"$assetId\"',
+    "assetType": "Application",
+    "applicationType": "Financial",
+    "applicationName": "Test app 1",
+    "applicationBuildName": "test-build",
+    "soxFinancial": true,
+    "ppi": true,
+    "mnpi": true,
+    "infoClass": "Restricted",
+    "customerFacing": true,
+    "externallyFacing": true,
+    "assetTier": "Tier 01",
+    "fairLending": true
+}');
 
-else 
-    echo $signupResponse;
+if [ "$onBoardingResponse" = "TPI Data created/updated successfully" ] ; then
+    wget https://sigdevsecops.blob.core.windows.net/intelligence-orchestration/2021.01/io-manifest.yml
+    workflow=$(cat io-manifest.yml | sed " s~<<ASSET_ID>>~$assetId~g; s~<<APP_ID>>~$assetId~g")
+    # apply the yml with the substituted value
+    echo "$workflow" >io-manifest.yml
+
+    echo "Save the following values for further use:"
+    echo "assetId: $assetId"
+    echo "IO_ACCESS_TOKEN: $userToken"
+    echo "INFO: io-manifest.yml is generated. Please update the source code management details in it and add the file to the root of the project."
+else
+    echo $onBoardingResponse;
     exit 1;
 fi
+
+
